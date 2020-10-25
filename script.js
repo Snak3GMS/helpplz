0d8b3a7d1db8c3c5ec03b4ef419d562a9ecd758f
Snak3GMS <vova.shkar74@gmail.com>
2020-10-25 19:41:59 +0300

    minor

--- script.js
+++ script.js
@@ -10,14 +10,17 @@ $(function(){
         if ($('#inName').val() == '') return false;
         else {
         $('.listnull').remove();
-        $('.todolist-wrapper').append('<div class="list__item"><div class="wrapper"><div class="item-title">'+$('#inName').val()+'</div><a class="item-delete"><div></div><div></div></a></div><div class="descr_todo">'+$('#inDescr').val()+'</div></div>');
-        }
+        $('.todolist-wrapper').append('<div class="list__item" style="display: none;"><div class="wrapper"><div class="item-title">'+$('#inName').val()+'</div><a class="item-delete"><div></div><div></div></a></div><div class="descr_todo">'+$('#inDescr').val()+'</div></div>');
+        $('.list__item').fadeIn();
+    }
         $('#inName').val('').focus();
         $('#inDescr').val('');
     });
     $('html').on('click', '.item-delete', function(){
-        $(this).parents('.list__item').remove();
-        if ($('.list__item').length == false) $('.todolist-wrapper').append('<div class="listnull">Список пуст...</div></div>');
+        $(this).parents('.list__item').fadeOut(400, function(){$(this).remove();});
+        function go(){
+        if ($('.list__item').length == false) $('.todolist-wrapper').append('<div class="listnull">Список пуст...</div></div>');}
+        setTimeout (go, 500)
     });
     $("#inDescr").keydown(function(event){
         if (event.shiftKey && (event.keyCode == 13)) {}
