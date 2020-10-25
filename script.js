69918282a369a9fcd2540ca4168e6a220fe75cf4
Snak3GMS <vova.shkar74@gmail.com>
2020-10-25 13:21:11 +0300

    Fix some Bug

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(function(){
     $('.button').click(function(){
+        if ($('#inName').val() == '') return false;
+        else {
         $('.listnull').remove();
         $('.todolist-wrapper').append('<div class="list__item"><div class="wrapper"><div class="item-title">'+$('#inName').val()+'</div><a class="item-delete"><div></div><div></div></a></div><div class="descr_todo">'+$('#inDescr').val()+'</div></div>');
+        }
         $('#inName').val('');
         $('#inDescr').val('');
     });
