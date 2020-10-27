af504bca7729ab7bade5c48c3c8e5b755f153c29
Snak3GMS <36984325+Snak3GMS@users.noreply.github.com>
2020-10-27 09:11:16 +0300

    add localStorage

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(function(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let item = localStorage.getItem(key);
+        $('.listnull').remove();
+        $('.todolist-wrapper').append('<div class="list__item" style="display: none;"><div class="wrapper"><div class="item-title">'+key+'</div><a class="item-delete"><div></div><div></div></a></div><div class="descr_todo">'+item+'</div></div>');
+        $('.list__item').fadeIn();
+}
+    
     $("#inName").keydown(function(event){
         if(event.keyCode == 13){
             $('#inDescr').focus();
@@ -12,11 +20,13 @@ $(function(){
         $('.listnull').remove();
         $('.todolist-wrapper').append('<div class="list__item" style="display: none;"><div class="wrapper"><div class="item-title">'+$('#inName').val()+'</div><a class="item-delete"><div></div><div></div></a></div><div class="descr_todo">'+$('#inDescr').val()+'</div></div>');
         $('.list__item').fadeIn();
+        localStorage.setItem($('#inName').val(), $('#inDescr').val());
     }
         $('#inName').val('').focus();
         $('#inDescr').val('');
     });
     $('html').on('click', '.item-delete', function(){
+        localStorage.removeItem($(this).parents('.list__item').find('.item-title').html());
         $(this).parents('.list__item').fadeOut(400, function(){$(this).remove();});
         function go(){
         if ($('.list__item').length == false) $('.todolist-wrapper').append('<div class="listnull">Список пуст...</div></div>');}
